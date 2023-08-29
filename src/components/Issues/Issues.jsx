026060f14c454e2d3cb03e1846eb88be1de4a243
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 01:27:56 +0900

    feat: 이슈 데이터를 바탕으로 리스트 생성

--- src/components/Issues/Issues.jsx
+++ src/components/Issues/Issues.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Issue from '../Issue/Issue';
 
 function Issues() {
-  return <div>Issues</div>;
+  const selector = useSelector(state => state.issues.issuesStore);
+
+  return (
+    <ul>
+      {selector?.map((issues, idx) => {
+        return <Issue key={issues.id} data={issues} issueCount={idx} />;
+      })}
+    </ul>
+  );
 }
 
 export default Issues;
