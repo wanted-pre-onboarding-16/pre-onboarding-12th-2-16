90dc0509d27af2655cfe25b84b32ea2b15943b4b
SeungGukYoo <dbtmd878@gmail.com>
2023-08-29 23:28:33 +0900

    feat: axios 모듈화

---
+++ src/util/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const TOKEN = import.meta.env.VITE_GIT_TOKEN;
+
+export const customAxios = axios.create({
+  baseURL: 'https://api.github.com/repos/facebook/react/issues',
+  headers: {
+    Accept: 'application/vnd.github+json',
+    Authorization: `Bearer ${TOKEN}`,
+    'X-GitHub-Api-Version': '2022-11-28',
+  },
+});
