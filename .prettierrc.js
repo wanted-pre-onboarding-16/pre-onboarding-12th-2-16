90044ca03c1899bdb225b793d1ff64c2f37ebbc0
ovelute53 <ovelute53@naver.com>
2023-08-29 19:08:44 +0900

    vite project start && preset setting

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// .prettierrc.js
+
+module.exports = {
+  printWidth: 100, // printWidth default 80 => 100 으로 변경
+  singleQuote: true, // "" => ''
+  arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
+  semi: true, // 세미콜론 사용 여부
+  tabWidth: 2, // 탭 너비 2
+};
