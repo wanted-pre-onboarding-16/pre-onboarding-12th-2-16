ed586b4b364bade39854d107fb571fd3386c04a3
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 00:18:53 +0900

    feat: 프로젝트에 리덕스 툴킷 연결

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import { store } from './store.js';
 
 const router = createBrowserRouter([{ path: '/', element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
