f8337787d614fff187c4118dbcc468dc43efdcef
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 23:27:16 +0900

    feat: 이슈 불러오는 함수와 스크롤 이벤트 추가

--- src/components/Issues/Issues.jsx
+++ src/components/Issues/Issues.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { callIssueReducer } from '../../slice/issuesSlice';
 import Issue from '../Issue/Issue';
 
 function Issues() {
+  const dispatch = useDispatch();
   const selector = useSelector(state => state.issue);
 
+  useEffect(() => {
+    dispatch(
+      callIssueReducer({
+        lastIssueNumber: selector.lastIssueNumber,
+        nextIssuePage: selector.nextIssuePage,
+      }),
+    );
+  }, [dispatch]);
+
+  useEffect(() => {
+    const onScroll = () => {
+      let currentHeight = window.scrollY + document.documentElement.clientHeight;
+      let restScrollHeigth = document.documentElement.scrollHeight - 600;
+
+      if (currentHeight > restScrollHeigth && !selector.isLoading) {
+        dispatch(
+          callIssueReducer({
+            lastIssueNumber: selector.lastIssueNumber,
+            nextIssuePage: selector.nextIssuePage,
+          }),
+        );
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [dispatch, selector.isLoading]);
   return (
     <ul>
       {selector.error}
