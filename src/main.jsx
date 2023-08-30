325488919c1e73811ec643199e42a3dfff821043
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 23:25:25 +0900

    feat: 하위 라우팅 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import Issues from './components/Issues/Issues.jsx';
 import './index.css';
+import Description from './pages/Description/Description.jsx';
 import { store } from './store.js';
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Issues />,
+      },
+      {
+        path: '/:id',
+        element: <Description />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
