012039c7746ca8e9fae56f1cf87dc6001b5ba7d0
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 23:24:55 +0900

    refactor: 이슈 호출 함수와 이벤트 함수를 Issues컴포넌트로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
-import Issues from './components/Issues/Issues';
+import { Outlet } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import { callIssueReducer } from './slice/issuesSlice';
 
 function App() {
-  const dispatch = useDispatch();
-  const selector = useSelector(state => state.issue);
-
-  useEffect(() => {
-    dispatch(
-      callIssueReducer({
-        lastIssueNumber: selector.lastIssueNumber,
-        nextIssuePage: selector.nextIssuePage,
-      }),
-    );
-  }, [dispatch]);
-
-  useEffect(() => {
-    const onScroll = () => {
-      let currentHeight = window.scrollY + document.documentElement.clientHeight;
-      let restScrollHeigth = document.documentElement.scrollHeight - 600;
-
-      if (currentHeight > restScrollHeigth && !selector.isLoading) {
-        dispatch(
-          callIssueReducer({
-            lastIssueNumber: selector.lastIssueNumber,
-            nextIssuePage: selector.nextIssuePage,
-          }),
-        );
-      }
-    };
-
-    window.addEventListener('scroll', onScroll);
-
-    return () => window.removeEventListener('scroll', onScroll);
-  }, [dispatch, selector.isLoading]);
-
   return (
-    <>
-      <Layout />
-      <Issues />
-    </>
+    <Layout>
+      <Outlet />
+    </Layout>
   );
 }
 
