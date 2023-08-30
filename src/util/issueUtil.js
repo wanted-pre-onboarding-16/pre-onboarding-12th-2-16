6520a2f4622e59374753aed626fbfc081aaabe58
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 16:00:37 +0900

    refactor: 전개 연산자 코드로 변경

--- src/util/issueUtil.js
+++ src/util/issueUtil.js
@@ -1,11 +1,11 @@
 import { customAxios } from './api';
 
-export function closerGetIssue(per_page) {
+export function closerGetIssue({ lastIssueNumber, nextIssuePage }) {
   return {
     async promise() {
       const result = await customAxios({
         method: 'get',
-        params: { per_page },
+        params: { per_page: lastIssueNumber, page: nextIssuePage },
       });
 
       return result.data;
