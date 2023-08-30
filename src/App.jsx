881425867c31f0b6d979ee4606adb32c6043e4a9
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 20:06:44 +0900

    feat: 무한 스크롤 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,40 @@ import './App.css';
 
 import Issues from './components/Issues/Issues';
 import Layout from './components/Layout/Layout';
-import { fetchByIssues } from './slice/issuesSlice';
+import { callIssueReducer } from './slice/issuesSlice';
 
 function App() {
   const dispatch = useDispatch();
   const selector = useSelector(state => state.issue);
 
-  console.info(selector);
   useEffect(() => {
     dispatch(
-      fetchByIssues({
+      callIssueReducer({
         lastIssueNumber: selector.lastIssueNumber,
         nextIssuePage: selector.nextIssuePage,
       }),
     );
   }, [dispatch]);
+
+  useEffect(() => {
+    const onScroll = () => {
+      let currentHeight = window.scrollY + document.documentElement.clientHeight;
+      let restScrollHeigth = document.documentElement.scrollHeight - 600;
+
+      if (currentHeight > restScrollHeigth && !selector.isLoading) {
+        dispatch(
+          callIssueReducer({
+            lastIssueNumber: selector.lastIssueNumber,
+            nextIssuePage: selector.nextIssuePage,
+          }),
+        );
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [dispatch, selector.isLoading]);
 
   return (
     <>
