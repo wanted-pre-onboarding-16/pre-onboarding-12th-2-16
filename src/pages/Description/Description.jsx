8056c22e2ddacdafc424c1340922f7c1e8c3894b
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 23:29:02 +0900

    feat: 마크다운 파싱하여 html로 출력하는 컴포넌트 추가

--- src/pages/Description/Description.jsx
+++ src/pages/Description/Description.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import App from '../../App';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import remarkHtml from 'remark-html';
+import remarkParse from 'remark-parse';
+import { unified } from 'unified';
 
 function Description() {
-  return <App>Description</App>;
+  const [markdownText, setMarkdownText] = useState('');
+  const selector = useSelector(state => state.issue.issuesStore);
+  const param = useParams();
+  useEffect(() => {
+    const markdownChangeHtml = async () => {
+      const { value } = await unified()
+        .use(remarkParse)
+        .use(remarkHtml)
+        .process(selector[param.id]?.body);
+      setMarkdownText(value);
+    };
+    setMarkdownText('');
+    markdownChangeHtml();
+  }, [param.id, selector]);
+  const createMarkdown = useCallback(() => {
+    return { __html: markdownText };
+  }, [markdownText]);
+
+  return <div dangerouslySetInnerHTML={createMarkdown()} />;
 }
 
 export default Description;
