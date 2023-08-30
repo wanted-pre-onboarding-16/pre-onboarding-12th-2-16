343d3a6d15d4dc7a852e512ae4abaece38a4f9bf
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 15:59:06 +0900

    feat: 이슈를 갖고오기 위한 이슈 페이지 번호 추가

--- src/slice/issuesSlice.js
+++ src/slice/issuesSlice.js
@@ -3,15 +3,19 @@ import { closerGetIssue } from '../util/IssueUtil';
 
 const initialState = {
   issuesStore: [],
-  lastPageCount: 10,
+  lastIssueNumber: 10,
+  nextIssuePage: 1,
   error: '',
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
 };
 
 export const fetchByIssues = createAsyncThunk(
   'issue/fetchByIssues',
-  async (_, { rejectWithValue }) => {
-    const result = await closerGetIssue(initialState.lastPageCount).promise();
-    console.info(result, 'result');
+  async (pageInfo, { rejectWithValue }) => {
+    const result = await closerGetIssue(pageInfo).promise();
+
     if (!result) {
       return rejectWithValue('이슈가 없습니다!');
     }
@@ -24,18 +28,26 @@ export const issuesSlice = createSlice({
   initialState,
   extraReducers: builder => {
     builder.addCase(fetchByIssues.pending, (state, action) => {
-      console.info(action.payload, 'pending');
+      state.isLoading = true;
+      state.isSuccess = false;
+      state.isError = false;
     });
     builder.addCase(fetchByIssues.fulfilled, (state, action) => {
-      state.issuesStore = state.issuesStore.concat(action.payload);
-      state.lastPageCount += 10;
+      const concatArr = state.issuesStore.concat(action.payload);
+      state.issuesStore = concatArr;
+      state.lastIssueNumber += 10;
+      if (state.lastIssueNumber % 30 === 0) {
+        state.nextIssuePage += 1;
+      }
+      state.isLoading = false;
+      state.isSuccess = true;
     });
     builder.addCase(fetchByIssues.rejected, (state, action) => {
       state.error = action.payload;
+      state.isLoading = false;
+      state.isError = true;
     });
   },
 });
-
-export const { getIssuesData } = issuesSlice.actions;
 
 export default issuesSlice.reducer;
