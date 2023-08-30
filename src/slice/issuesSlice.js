63bf0f5a8e155da7bb164efcb6cefbbde1da2c36
SeungGukYoo <dbtmd878@gmail.com>
2023-08-30 14:13:38 +0900

    feat: createAsyncThunk 통한 이슈 호출기능 추가

--- src/slice/issuesSlice.js
+++ src/slice/issuesSlice.js
@@ -1,16 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { closerGetIssue } from '../util/IssueUtil';
 
 const initialState = {
   issuesStore: [],
+  lastPageCount: 10,
+  error: '',
 };
 
+export const fetchByIssues = createAsyncThunk(
+  'issue/fetchByIssues',
+  async (_, { rejectWithValue }) => {
+    const result = await closerGetIssue(initialState.lastPageCount).promise();
+    console.info(result, 'result');
+    if (!result) {
+      return rejectWithValue('이슈가 없습니다!');
+    }
+    return result;
+  },
+);
+
 export const issuesSlice = createSlice({
-  name: 'issues',
+  name: 'issue',
   initialState,
-  reducers: {
-    getIssuesData: (state, action) => {
-      state.issuesStore = action.payload;
-    },
+  extraReducers: builder => {
+    builder.addCase(fetchByIssues.pending, (state, action) => {
+      console.info(action.payload, 'pending');
+    });
+    builder.addCase(fetchByIssues.fulfilled, (state, action) => {
+      state.issuesStore = state.issuesStore.concat(action.payload);
+      state.lastPageCount += 10;
+    });
+    builder.addCase(fetchByIssues.rejected, (state, action) => {
+      state.error = action.payload;
+    });
   },
 });
 
