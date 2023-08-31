c8dbea808c22c8fbb4b3b53cad5d3aff37bd81e5
ovelute53 <ovelute53@naver.com>
2023-08-31 14:14:01 +0900

    styles: GlobalStyles created && adapted

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 
 import { Outlet } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 function App() {
   return (
     <Layout>
+      <GlobalStyles />
       <Outlet />
     </Layout>
   );
