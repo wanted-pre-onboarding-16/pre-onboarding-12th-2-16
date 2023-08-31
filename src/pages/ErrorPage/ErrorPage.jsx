86d363071de4fd54a51eb987c2d83fd073a797e6
ovelute53 <ovelute53@naver.com>
2023-08-31 16:43:08 +0900

    feat: ErrorPage simple style

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,34 @@
-export function ErrorPage() {
+import React from 'react';
+import styled from 'styled-components';
+
+function ErrorPage({ message = 'Oops! Something went wrong.' }) {
   return (
-    <div>
-      <h3>Not Found</h3>
-    </div>
+    <ErrorContainer>
+      <ErrorTitle>Error</ErrorTitle>
+      <ErrorMessage>{message}</ErrorMessage>
+    </ErrorContainer>
   );
 }
+
+export default ErrorPage;
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f9f9f9;
+`;
+
+const ErrorTitle = styled.h1`
+  font-size: 3rem;
+  color: #e74c3c;
+  margin-bottom: 1rem;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 1.5rem;
+  color: #555;
+  text-align: center;
+`;
