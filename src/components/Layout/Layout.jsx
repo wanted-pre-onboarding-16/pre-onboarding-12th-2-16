6a237fa06fd2b06aff382078a68b27948ebdf539
SeungGukYoo <dbtmd878@gmail.com>
2023-08-31 16:42:41 +0900

    style: 글로벌 레이아웃 스타일링 및 적용

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { styled } from 'styled-components';
 import { GlobalStyles } from '../../styles/GlobalStyles';
 
 function Layout({ children }) {
   return (
-    <>
+    <Container>
       <GlobalStyles />
-      <h1>Facebook / React</h1>
+      <Header>Facebook / React</Header>
       {children}
-    </>
+    </Container>
   );
 }
 
 export default Layout;
+
+const Container = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+`;
+const Header = styled.h1`
+  margin-bottom: 40px;
+`;
