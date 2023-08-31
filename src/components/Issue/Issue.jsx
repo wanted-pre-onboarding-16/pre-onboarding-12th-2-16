e980f83733130b9cc29fcf110fcf79e71fedb729
SeungGukYoo <dbtmd878@gmail.com>
2023-08-31 16:41:43 +0900

    feat: date 정규식으로 변환 후 CSS 작업

--- src/components/Issue/Issue.jsx
+++ src/components/Issue/Issue.jsx
@@ -7,30 +7,38 @@ function Issue({ data, issueCount }) {
   const moveToDescription = () => {
     navigation(`${issueCount}`);
   };
-  const moveToAdvertosement = e => {
+  const moveToAdvertosement = () => {
     window.location.href = 'https://www.wanted.co.kr/';
   };
 
+  const DateFormatter = data.updated_at.match(/(\d{4})-(\d{2})-(\d{2})T/);
+  let DateFormatterArr = `${DateFormatter[1]}년 ${DateFormatter[2]}월 ${DateFormatter[3]}일`;
   return (
     <LiTag>
       {(issueCount + 1) % 5 === 0 && (
-        <div onClick={moveToAdvertosement}>
+        <AdverTisement onClick={moveToAdvertosement}>
           <img
             src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
             alt="adevertisement logo"
           />
-        </div>
+        </AdverTisement>
       )}
-      <div onClick={moveToDescription}>
-        <p>
-          <span>#{data.number}</span>
-          title: {data.title}
-        </p>
-        <p>
-          작성자: {data.user.login} 작성일:{data.updated_at}
-        </p>
-      </div>
-      <div>코멘트: {data.comments}</div>
+      <IssueContainer onClick={moveToDescription}>
+        <IssueLeftSide>
+          <LeftSideTitle>
+            <LeftSideTitleSpan>#{data.number}</LeftSideTitleSpan>
+            {data.title}
+          </LeftSideTitle>
+          <LeftSideWriter>
+            <LeftSideWriterSpan>작성자: {data.user.login}</LeftSideWriterSpan> 작성일:
+            {DateFormatterArr}
+          </LeftSideWriter>
+        </IssueLeftSide>
+
+        <RightSide>
+          <p>코멘트: {data.comments}</p>
+        </RightSide>
+      </IssueContainer>
     </LiTag>
   );
 }
@@ -38,7 +46,52 @@ function Issue({ data, issueCount }) {
 export default Issue;
 
 const LiTag = styled.li`
-  margin: 20px 0;
+  border-bottom: 1px solid gray;
   padding: 20px 0;
-  border-bottom: 2px solid #eee;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+const IssueLeftSide = styled.div`
+  text-align: left;
+  padding: 20px;
+  width: 650px;
+`;
+const LeftSideTitle = styled.h1`
+  margin-bottom: 20px;
+  font-size: 18px;
+`;
+const LeftSideTitleSpan = styled.span`
+  margin-right: 20px;
+`;
+const LeftSideWriter = styled.p`
+  width: 100%;
+`;
+const LeftSideWriterSpan = styled.span`
+  margin-right: 5px;
+`;
+const RightSide = styled.div`
+  margin: auto 4px;
+`;
+const AdverTisement = styled.div`
+  padding: 20px 0;
+
+  margin-bottom: 20px;
+
+  border: 1px solid gray;
+  transition: 0.3s ease;
+  &:hover {
+    background-color: rgba(0, 128, 255, 0.1);
+  }
+`;
+const IssueContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  transition: 0.3s ease;
+  &:hover {
+    background-color: rgba(0, 128, 255, 0.1);
+    box-shadow:
+      0 5px 15px rgba(0, 128, 255, 0.1),
+      0 -5px 15px rgba(0, 128, 255, 0.1); /* 위, 아래로 흐림 효과 */
+  }
 `;
